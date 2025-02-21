774e897f32896288c2c0f21447be5f2df62b9051
RhexNiebres <rhexniebres@gmail.com>
2025-02-20 16:14:10 -0800

    import bookRouter.js into file

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const authorRouter = require("./routes/authorRouter")
+const booksRouter = require("./routes/bookRouter")
 
-app.use("/authors", authorRouter)
+app.use("/authors", authorRouter);
+app.use("/books", booksRouter);
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
