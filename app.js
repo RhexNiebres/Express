ea962361316c6246380cfa37d99be679110b193b
RhexNiebres <rhexniebres@gmail.com>
2025-02-20 16:12:02 -0800

    import authorRoute.js into file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
+const authorRouter = require("./routes/authorRouter")
 
-app.get("/", (req, res) => res.send("Hello, world!"));
-
+app.use("/authors", authorRouter)
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
