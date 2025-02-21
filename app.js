9b52330d7f7c35427a509c62a81b4684b3df5b68
RhexNiebres <rhexniebres@gmail.com>
2025-02-20 19:53:45 -0800

    add custome error handling

--- app.js
+++ app.js
@@ -20,5 +20,6 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send(err);
+   // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+   res.status(err.statusCode || 500).send(err.message);
 });
