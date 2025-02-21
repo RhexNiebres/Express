abd121d7e8d007ae14c7a42bc0df8f9c1e00dc69
RhexNiebres <rhexniebres@gmail.com>
2025-02-21 13:44:45 -0800

    Fix: inconsistent spacing, indentation, and line breaks of file

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,17 +2,15 @@ const CustomNotFoundError = require("../errors/CustomNotFoundError");
 const asyncHandler = require("express-async-handler");
 const db = require("../db");
 
-const getAuthorById = asyncHandler(async(req, res) => {
+const getAuthorById = asyncHandler(async (req, res) => {
   const { authorId } = req.params;
-
 
   const author = await db.getAuthorById(Number(authorId));
 
   if (!author) {
-   throw new CustomNotFoundError('Author Not Found')
+    throw new CustomNotFoundError("Author Not Found");
   }
-  res.send(`Author Name: ${author.name}`)
+  res.send(`Author Name: ${author.name}`);
 });
-
 
 module.exports = { getAuthorById };
