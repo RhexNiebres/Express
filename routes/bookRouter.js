32ce7269ae9ed7fd79bc8e7145eba1e80f750eaf
RhexNiebres <rhexniebres@gmail.com>
2025-02-21 13:45:22 -0800

    Fix: inconsistent spacing, indentation, and line breaks of file

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -1,11 +1,11 @@
-const { Router } = require("express")
+const { Router } = require("express");
 
 const booksRouter = Router();
 
 booksRouter.get("/", (req, res) => res.send("ALL Books"));
 booksRouter.get("/:booksId", (req, res) => {
-    const { booksId } = req.params;
-    res.send(`Books Id: ${booksId}`)
+  const { booksId } = req.params;
+  res.send(`Books Id: ${booksId}`);
 });
 
 module.exports = booksRouter;
