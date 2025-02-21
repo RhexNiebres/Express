1aa58d9f1428b0ae34dd515f53ed9679a656a793
RhexNiebres <rhexniebres@gmail.com>
2025-02-20 19:07:21 -0800

    import author conteoller and comment out unused code

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require("express")
 
+const { getauthorById } = require("../controllers/authorController")//import controller
 const authorRouter = Router();
 
 authorRouter.get("/", (req, res) => res.send("ALL authors"));
-authorRouter.get("/:authorId", (req, res) => {
-    const { authorId } = req.params;
-    res.send(`Author Id: ${authorId}`)
-});
+authorRouter.get("/:authorId", getauthorById);
+
+// authorRouter.get("/:authorId", (req, res) => {
+//     const { authorId } = req.params;
+//     res.send(`Author Id: ${authorId}`)
+// });
 
 module.exports = authorRouter;
