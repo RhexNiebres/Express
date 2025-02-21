6110b6d2f35656af152075baaa35de717089cf8a
RhexNiebres <rhexniebres@gmail.com>
2025-02-20 16:22:36 -0800

    import indexRoute.js to be the defualt server route

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 const authorRouter = require("./routes/authorRouter")
-const booksRouter = require("./routes/bookRouter")
+const bookRouter = require("./routes/bookRouter")
+const indexRouter = require("./routes/indexRouter")
 
 app.use("/authors", authorRouter);
-app.use("/books", booksRouter);
+app.use("/books", bookRouter);
+app.use("/", indexRouter);
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port ${PORT}!`);
