c3926307e498a88f73b1e012b2689bf149143d74
RhexNiebres <rhexniebres@gmail.com>
2025-02-21 17:00:54 -0800

    add dynamic reder list

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
-const indexRouter = require("./routes/indexRouter");
+// const indexRouter = require("./routes/indexRouter");
 const path = require("node:path");
 
 
@@ -11,7 +11,23 @@ app.set("view engine", "ejs");
 
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
-app.use("/", indexRouter);
+// app.use("/", indexRouter); 
+
+// app.get("/", (req, res) => {
+//   res.render("index", { message: "EJS rocks!" });
+// });
+
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
